2bd4b317f83f90ed86b7db4bd11c3df3ad589aa6
Khoa Nguyen Dang1 <khoa.dang1.nguyen@dksh.com>
2026-04-08 10:25:37 +0700

    update requirement

--- src/hello_world/defs/assets/print.py
+++ src/hello_world/defs/assets/print.py
@@ -1,17 +1,26 @@
 import dagster as dg
 from pathlib import Path
 from hello_world.resources.send_ntfy import NftyResource
+import requests
+from datetime import datetime
 
+TODAY = datetime.today().date().strftime(format='%Y-%M-%d')
 
 @dg.asset
-def hello(nfty: NftyResource) -> None: 
+def hello(
+          context : dg.AssetExecutionContext) -> None: 
     
     try: 
-        print('hello')
-        nfty.success_message()
+        context.log.info(f"Đang run asset Hello cho ngày {TODAY}")
+        prrint("Hello")
+        # nfty.success_message()
     except Exception as e:
-        nfty.failure_message()
-        raise e
+         
+         ERROR = f"asset hello run cho ngày {TODAY} lỗi: {str(e)}"
+         context.log.info(ERROR)
+         context.log.info("Đang gửi lỗi")
+         requests.post("https://ntfy.sh/dagster_hello_worlds", data = ERROR.encode(encoding='utf-8'), verify=False)
+         context.log.info("Gửi lỗi thành công")
 
 
 
@@ -19,10 +28,11 @@ def hello(nfty: NftyResource) -> None:
 def world(nfty: NftyResource) -> None:
     try: 
         print('world')
-        nfty.success_message()
+        SUCCESS = "data run thành công"
+        nfty.success_message(SUCCESS)
     except Exception as e:
-        nfty.failure_message()
-        raise e
+        ERROR = str(e)
+        nfty.failure_message(ERROR)
 
 
 
